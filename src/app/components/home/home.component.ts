008bd6ba940eb59808272b2d84cc6bbc4ec8eb52
Jucalino <joseluis.bmjnet@gmail.com>
2025-04-16 11:26:48 -0300

    Templates e data binding

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,5 +9,10 @@ import { FooterComponent } from "../footer/footer.component";
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  name = "Juca"
+  catUrl = "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg"
+  
+  teste(){
+    console.log("clicou")
+  }
 }
